f2469f02f05781f0da4781e98e101a8198313e07
ZGSLZL <45186889+ZGSLZL@users.noreply.github.com>
2020-06-03 11:25:46 +0800

    Add files via upload
    Fix bug caused by missing 'with fluid.dygraph.guard()' in convert.py

--- convert.py
+++ convert.py
@@ -1,5 +1,7 @@
+import paddle.fluid as fluid
 from models.resnet import ResNet
 from utils.util import torch_weight_to_paddle_model
 
-resnet18 = ResNet(depth=18)
-torch_weight_to_paddle_model('./pretrained/resnet18-5c106cde.pth', resnet18)
+with fluid.dygraph.guard():
+    resnet18 = ResNet(depth=18, frozen_stages=-1, norm_cfg=dict(type='BN'))
+    torch_weight_to_paddle_model('./pretrained/resnet18-5c106cde.pth', resnet18)
