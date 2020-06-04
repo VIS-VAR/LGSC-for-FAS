d28eae49883674b3468691f4a11caed2040d886a
ZGSLZL <45186889+ZGSLZL@users.noreply.github.com>
2020-06-04 10:40:42 +0800

    Add files via upload

--- utils/util.py
+++ utils/util.py
@@ -1,5 +1,5 @@
 import os
-# import torch
+import torch
 import json
 import paddle.fluid as fluid
 import matplotlib.pyplot as plt
@@ -7,33 +7,33 @@ import matplotlib.pyplot as plt
 resnet18 = 'https://download.pytorch.org/models/resnet18-5c106cde.pth'
 
 
-# def torch_weight_to_paddle_model(torch_weight_file, paddle_model):
-#     torch_weight = torch.load(torch_weight_file)
-#     fluid.dygraph.save_dygraph(paddle_model.state_dict(), './pretrained/resnet18-torch')
-#     paddle_weight, _ = fluid.dygraph.load_dygraph('./pretrained/resnet18-torch')
-#     for k, p in torch_weight.items():
-#         if k in paddle_weight:
-#             np_parm = torch_weight[k].detach().numpy()
-#             if np_parm.shape == paddle_weight[k].shape:
-#                 paddle_weight[k] = np_parm
-#             else:
-#                 print('torch parm {} dose not match paddle parm {}'.format(k, k))
-#         elif 'running_mean' in k:
-#             np_parm = torch_weight[k].detach().numpy()
-#             if np_parm.shape == paddle_weight[k[:-12]+'_mean'].shape:
-#                 paddle_weight[k[:-12]+'_mean'] = np_parm
-#             else:
-#                 print('torch parm {} dose not match paddle parm {}'.format(k, k[:-12]+'_mean'))
-#         elif 'running_var' in k:
-#             np_parm = torch_weight[k].detach().numpy()
-#             if np_parm.shape == paddle_weight[k[:-11] + '_variance'].shape:
-#                 paddle_weight[k[:-11] + '_variance'] = np_parm
-#             else:
-#                 print('torch parm {} dose not match paddle parm {}'.format(k, k[:-11] + '_variance'))
-#         else:
-#             print('torch parm {} not exist in paddle modle'.format(k))
-#     paddle_model.set_dict(paddle_weight)
-#     fluid.dygraph.save_dygraph(paddle_model.state_dict(), './pretrained/resnet18-torch')
+def torch_weight_to_paddle_model(torch_weight_file, paddle_model):
+    torch_weight = torch.load(torch_weight_file)
+    fluid.dygraph.save_dygraph(paddle_model.state_dict(), './pretrained/resnet18-torch')
+    paddle_weight, _ = fluid.dygraph.load_dygraph('./pretrained/resnet18-torch')
+    for k, p in torch_weight.items():
+        if k in paddle_weight:
+            np_parm = torch_weight[k].detach().numpy()
+            if np_parm.shape == paddle_weight[k].shape:
+                paddle_weight[k] = np_parm
+            else:
+                print('torch parm {} dose not match paddle parm {}'.format(k, k))
+        elif 'running_mean' in k:
+            np_parm = torch_weight[k].detach().numpy()
+            if np_parm.shape == paddle_weight[k[:-12]+'_mean'].shape:
+                paddle_weight[k[:-12]+'_mean'] = np_parm
+            else:
+                print('torch parm {} dose not match paddle parm {}'.format(k, k[:-12]+'_mean'))
+        elif 'running_var' in k:
+            np_parm = torch_weight[k].detach().numpy()
+            if np_parm.shape == paddle_weight[k[:-11] + '_variance'].shape:
+                paddle_weight[k[:-11] + '_variance'] = np_parm
+            else:
+                print('torch parm {} dose not match paddle parm {}'.format(k, k[:-11] + '_variance'))
+        else:
+            print('torch parm {} not exist in paddle modle'.format(k))
+    paddle_model.set_dict(paddle_weight)
+    fluid.dygraph.save_dygraph(paddle_model.state_dict(), './pretrained/resnet18-torch')
 
 
 def model_size(model):
